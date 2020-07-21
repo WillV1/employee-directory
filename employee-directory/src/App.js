312792d097b747bbe5a22d7ae1f64e3013059874
WillV1 <wmvanhook@gmail.com>
2020-07-21 12:30:11 -0400

    Upload for review

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -9,33 +9,6 @@ import SearchBar from './components/SearchBar';
 
 import './App.css';
 
-const employees = [
-  {
-    id: 1,
-    name: "Mike",
-    email: '123@gmail.com',
-    phone: '919-123-4567'
-  },
-  {
-    id: 2,
-    name: "Joe",
-    email: '123@gmail.com',
-    phone: '919-123-4567'
-  },
-  {
-    id: 3,
-    name: "Sue",
-    email: '123@gmail.com',
-    phone: '919-123-4567'
-  },
-  {
-    id: 4,
-    name: "Billy",
-    email: '123@gmail.com',
-    phone: '919-123-4567'
-  }
-];
-
 
 class App extends React.Component {
 
@@ -46,28 +19,14 @@ class App extends React.Component {
 
 
   // componentDidMount() {
-  //   this.employeeGroup();
+  
   // }
 
   componentDidMount() {
-    // axios.get({
-    //   url: 'https://randomuser.me/api/?results=10',
-    //   dataType: 'json',
-    //   success: function(data) {
-    //     console.log(data);
-    //   }
-    // }).then(res => {
-    //   const employees = res.data;
-    //   this.setState({ filteredEmployees });
-    // })
-
     axios.get('https://randomuser.me/api/?results=10')
-     .then((response) => {
-       console.log(response);
-       this.setState({filteredEmployees})
-     })
-    .catch((error)=>{
-       console.log(error);
+    .then( (response) => {
+      let employees = response.data.results;
+      this.setState({ filteredEmployees: response.data.results });
     });
 
   };
@@ -76,22 +35,22 @@ class App extends React.Component {
     this.setState({ search: event.target.value });
   }
 
-  renderSearchList = () => {
-    const { search } = this.state
-    let filteredEmployees = employees.filter((employee) => { 
-      return employee.name.indexOf(search) >= 0;
-    })
+  // renderSearchList = () => {
+  //   const { search } = this.state
+  //   let filteredEmployees = employees.filter((employee) => {
+  //     return employee.name.indexOf(search) >= 0;
+  //   })
 
 
-    this.setState({filteredEmployees})
+  //   this.setState({ filteredEmployees })
 
 
-  }
+  // }
 
   handleFormSubmit = event => {
     event.preventDefault();
     this.renderSearchList();
-}
+  }
 
 
   render() {
@@ -103,8 +62,8 @@ class App extends React.Component {
           handleInputChange={this.updateSearch}
           handleFormSubmit={this.handleFormSubmit}
         />
-        <EmployeeTable employees={this.state.filteredEmployees} 
-        updateEmployeeList={this.updateSearch} 
+        <EmployeeTable employees={this.state.filteredEmployees}
+          updateEmployeeList={this.updateSearch}
         />
       </Container>
 
